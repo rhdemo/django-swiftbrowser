2b0260682b035511e1ee86441ee91e51f5a1fa85
zherman0 <zherman@redhat.com>
2018-04-12 12:15:27 -0600

    installation script tweaks

--- install-ui.sh
+++ install-ui.sh
@@ -11,6 +11,7 @@ then
     cp ./swiftbrowser/*.py /usr/lib/python2.7/site-packages/swiftbrowser/.
     cp ./swiftbrowser/templates/*.html /usr/lib/python2.7/site-packages/swiftbrowser/templates/.
     sed -i '50s/.*/SWIFT_HOST="'$SWIFT_HOST'"/' /usr/lib/python2.7/site-packages/swiftbrowser/settings.py
+    sed -i '49s/.*/SWIFT_VOLUME="'$SWIFT_VOLUME'"/' /usr/lib/python2.7/site-packages/swiftbrowser/settings.py
     cp storageui.service /etc/systemd/system/.
     chmod 664 /etc/systemd/system/storageui.service
     systemctl daemon-reload
@@ -18,5 +19,5 @@ then
     systemctl enable storageui
 
 else
-    echo "Host config file does not exist.  Please: cp host.config.example host.config and set the host name."
+    echo "Host config file does not exist.  Please: cp host.config.example host.config; vi host.config and set the host name."
 fi
